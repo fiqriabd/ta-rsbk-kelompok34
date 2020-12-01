a03ba3393655d9a4568428c40d1ae510494b28da
fiqriabd <fiqriabd1999@gmail.com>
2020-12-01 11:59:18 +0700

    update sesaat

---
+++ src/Komponen/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React, {} from 'react';
+import Tentang from '../Tentang/Tentang';
+
+export default function Home(){
+    return(
+            <div>
+                <Tentang />
+            </div>       
+    )
+}
